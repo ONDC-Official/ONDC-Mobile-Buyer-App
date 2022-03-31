20384ea2ab6bbfe76a8b6d06ab026ae0a17c3321
vrushali103 <vrushali@wemotiveforge.com>
2022-03-31 14:47:49 +0530

    add:splash screen in the app

---
+++ src/modules/authentication/splash/Splash.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Image, StyleSheet, View } from "react-native"
+import { appStyles } from "../../../styles/Styles"
+import { Text } from "react-native-elements"
+import {strings} from "../../../locales/i18n"
+
+const image=require("../../../assets/logo.png")
+const versionLabel=strings("global.version_label")
+
+
+
+const Splash=()=>{
+    return(
+    <View style={appStyles.container}>
+        <View style={[appStyles.container,styles.container]}>
+        <Image source={image} style={styles.image}/>
+        </View>
+        <View style={styles.footer}>
+            <Text>{versionLabel}</Text>
+        </View>
+       </View>
+    )
+}
+
+export default Splash
+
+const styles=StyleSheet.create({
+    container:{alignItems:"center",justifyContent:"center"},
+    image:{height:100,width:200},
+    footer:{alignItems:"center",marginBottom:20}
+})
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "../modules/authentication/login/Login";
+import Splash from "../modules/authentication/splash/Splash";
 
 const Stack=createStackNavigator()
 
@@ -11,7 +12,8 @@ const Navigation=()=>{
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
+            <Stack.Screen name="Splash" component={Splash} options={{headerShown:false}}/>
+            <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
             </Stack.Navigator>
 
         </NavigationContainer>
