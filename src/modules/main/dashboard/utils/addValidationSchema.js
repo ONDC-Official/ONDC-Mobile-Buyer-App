dbbc0502f6f007c75e4b7b3b9c0b8c0e04a6c854
Punit Vajpeyi <psvajpeyi@gmail.com>
2023-05-08 23:38:42 +0530

    BR-116: Updated validation for adress form.

--- src/modules/main/dashboard/utils/addValidationSchema.js
+++ src/modules/main/dashboard/utils/addValidationSchema.js
@@ -1,23 +1,23 @@
 import * as Yup from 'yup';
 
 export const validationSchema = Yup.object({
-  name: Yup.string().trim().required('This field is required'),
+  name: Yup.string().trim().required('Please enter Name'),
   email: Yup.string()
     .trim()
     .email('Please enter valid email address')
-    .required('This field is required'),
+    .required('Please enter Email'),
   number: Yup.string()
     .trim()
     .matches(/^[6-9]{1}[0-9]{9}$/, 'Invalid number')
-    .required('This field is required'),
-  city: Yup.string().trim().required('This field is required'),
-  state: Yup.string().trim().required('This field is required'),
+    .required('Please enter a valid Mobile Number'),
+  city: Yup.string().trim().required('City Name cannot be empty'),
+  state: Yup.string().trim().required('State Name cannot be empty'),
   pin: Yup.string()
     .trim()
     .matches(/^[1-9]{1}[0-9]{5}$/, 'Invalid pin code')
-    .required('This field is required'),
-  street: Yup.string().trim().required('This field is required'),
-  tag: Yup.string().trim().required('This field is required'),
+    .required('Please enter a valid Pin Code'),
+  street: Yup.string().trim().required('Please enter Full Address'),
+  tag: Yup.string().trim().required('Please select Address Type'),
 });
 
 export const addressTags = ['Home', 'Office', 'Other'];
