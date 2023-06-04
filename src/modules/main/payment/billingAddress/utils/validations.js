e977daaae1069dcfe7213a66556f91a91c9286c4
Punit Vajpeyi <psvajpeyi@gmail.com>
2023-06-04 10:41:34 +0530

    RATS-214: Updated validation message for update and add billing address.

--- src/modules/main/payment/billingAddress/utils/validations.js
+++ src/modules/main/payment/billingAddress/utils/validations.js
@@ -1,20 +1,20 @@
 import * as Yup from 'yup';
 
 export const billingAddressValidationSchema = Yup.object({
-  name: Yup.string().trim().required('This field is required'),
+  name: Yup.string().trim().required('Please enter Name'),
   email: Yup.string()
     .trim()
-    .email('Please enter valid email address')
-    .required('This field is required'),
+    .email('Please enter a valid email address')
+    .required('Please enter Email'),
   number: Yup.string()
     .trim()
-    .matches(/^[6-9]{1}[0-9]{9}$/, 'Invalid number')
-    .required('This field is required'),
-  city: Yup.string().trim().required('This field is required'),
-  state: Yup.string().trim().required('This field is required'),
+    .matches(/^[6-9]{1}[0-9]{9}$/, 'Please enter a valid Mobile Number')
+    .required('Please enter Mobile Number'),
+  city: Yup.string().trim().required('City Name cannot be empty'),
+  state: Yup.string().trim().required('State Name cannot be empty'),
   pin: Yup.string()
     .trim()
-    .matches(/^[1-9]{1}[0-9]{5}$/, 'Invalid pin code')
-    .required('This field is required'),
-  street: Yup.string().trim().required('This field is required'),
+    .matches(/^[1-9]{1}[0-9]{5}$/, 'Please enter a valid Pin Code')
+    .required('Please enter Pin Code'),
+  street: Yup.string().trim().required('Please enter Full Address'),
 });
