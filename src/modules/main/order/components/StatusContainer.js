abd016b71a01ac5f0b159faa4d93f26fa9b69e1d
Punit Vajpeyi <psvajpeyi@gmail.com>
2023-06-21 19:58:07 +0530

    RATS-249: Return Initaited staus updated

--- src/modules/main/order/components/StatusContainer.js
+++ src/modules/main/order/components/StatusContainer.js
@@ -8,7 +8,7 @@ const StatusContainer = ({product, theme}) => {
       <View style={styles.container}>
         <View style={styles.chipContainer}>
           <Chip selectedColor={theme.colors.red} mode="flat">
-            {product.return_status === 'Return_Initialted'
+            {product.return_status === 'Return_Initiated'
               ? 'Return Initiated'
               : product.return_status}
           </Chip>
