07ddd5a4f79ba9de456243286d914839d735ad46
Punit <psvajpeyi@gmail.com>
2024-04-08 12:22:59 +0530

    chg: Cross icon is smaller in app

--- src/components/bottomSheet/CloseSheetContainer.tsx
+++ src/components/bottomSheet/CloseSheetContainer.tsx
@@ -1,5 +1,5 @@
 import {Dimensions, StyleSheet, TouchableOpacity, View} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import React from 'react';
 import {useAppTheme} from '../../utils/theme';
 
@@ -16,14 +16,8 @@ const CloseSheetContainer = ({
   return (
     <View style={styles.container}>
       <View style={styles.closeSheet}>
-        <TouchableOpacity onPress={closeSheet}>
-          <View style={styles.closeButton} />
-          <Icon
-            name={'close-circle'}
-            color={theme.colors.neutral400}
-            size={32}
-            style={styles.closeIcon}
-          />
+        <TouchableOpacity onPress={closeSheet} style={styles.closeButton}>
+          <Icon name={'clear'} color={theme.colors.white} size={36} />
         </TouchableOpacity>
       </View>
       {children}
@@ -44,14 +38,12 @@ const makeStyles = (colors: any) =>
       paddingTop: 20,
     },
     closeButton: {
-      width: 20,
-      height: 20,
-      backgroundColor: colors.white,
+      width: 36,
+      height: 36,
+      backgroundColor: colors.neutral400,
       borderRadius: 32,
-      marginLeft: 6,
-    },
-    closeIcon: {
-      marginTop: -26,
+      alignItems: 'center',
+      justifyContent: 'center',
     },
   });
 
