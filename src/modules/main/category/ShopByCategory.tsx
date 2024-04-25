7e319fbb605b666491c9f5cee2b212286771bfd0
Punit <psvajpeyi@gmail.com>
2024-04-25 18:42:48 +0530

    chg: Grid Formatting missing in sub category view

--- src/modules/main/category/ShopByCategory.tsx
+++ src/modules/main/category/ShopByCategory.tsx
@@ -1,19 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import {FlatList, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useCallback, useEffect, useMemo} from 'react';
+import {FlatList, StyleSheet, TouchableOpacity, View} from 'react-native';
 import FastImage from 'react-native-fast-image';
 import {Text} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
+import {useTranslation} from 'react-i18next';
 import {PRODUCT_SUBCATEGORY} from '../../../utils/categories';
 import {useAppTheme} from '../../../utils/theme';
-import {useTranslation} from 'react-i18next';
 
 const ShopByCategory = ({route: {params}}: {route: any}) => {
   const {t} = useTranslation();
   const navigation = useNavigation<StackNavigationProp<any>>();
   const theme = useAppTheme();
   const styles = makeStyles(theme.colors);
-  const [subCategories, setSubCategories] = useState<any[]>([]);
 
   const navigateToSubCategory = (subCategory: any) => {
     navigation.navigate('SubCategoryDetails', {
@@ -22,8 +21,36 @@ const ShopByCategory = ({route: {params}}: {route: any}) => {
     });
   };
 
-  useEffect(() => {
-    setSubCategories(PRODUCT_SUBCATEGORY[params.category]);
+  const renderItem = useCallback(({item}: {item: any}) => {
+    if (item?.isEmpty) {
+      return <View style={styles.brand} />;
+    } else {
+      return (
+        <TouchableOpacity
+          style={styles.brand}
+          onPress={() => navigateToSubCategory(item)}>
+          <FastImage source={item?.imageUrl} style={styles.brandImage} />
+          <Text variant={'labelLarge'} style={styles.name}>
+            {t(`Product SubCategories.${item.key}`)}
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+  }, []);
+
+  const subCategories = useMemo(() => {
+    if (params.category) {
+      const list = PRODUCT_SUBCATEGORY[params.category];
+      const remainder = list.length % 3;
+      const emptyItems = remainder === 0 ? 0 : 3 - remainder;
+
+      // Create a new array with empty items
+      const dataWithEmptyItems = [...list];
+      for (let index = 0; index < emptyItems; index++) {
+        dataWithEmptyItems.push({isEmpty: true, key: String(index)});
+      }
+      return dataWithEmptyItems;
+    }
   }, [params.category]);
 
   useEffect(() => {
@@ -38,17 +65,7 @@ const ShopByCategory = ({route: {params}}: {route: any}) => {
       contentContainerStyle={styles.container}
       numColumns={3}
       data={subCategories}
-      renderItem={({item}) => (
-        <TouchableOpacity
-          key={item.key}
-          style={[styles.brand]}
-          onPress={() => navigateToSubCategory(item)}>
-          <FastImage source={item?.imageUrl} style={styles.brandImage} />
-          <Text variant={'labelLarge'} style={styles.name}>
-            {t(`Product SubCategories.${item.key}`)}
-          </Text>
-        </TouchableOpacity>
-      )}
+      renderItem={renderItem}
       keyExtractor={item => item.key}
     />
   );
