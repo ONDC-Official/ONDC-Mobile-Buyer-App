33bcabe4ab2d70ca627e7d1ba2fba828d1056fcf
Punit <psvajpeyi@gmail.com>
2024-07-31 21:10:44 +0530

    chg: filter for FBProducts

--- src/modules/main/provider/components/FBProducts.tsx
+++ src/modules/main/provider/components/FBProducts.tsx
@@ -1,18 +1,15 @@
 import React, {useEffect, useMemo, useRef, useState} from 'react';
-import {Text} from 'react-native-paper';
-import FastImage from 'react-native-fast-image';
 import axios from 'axios';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {useTranslation} from 'react-i18next';
 import {API_BASE_URL, CUSTOM_MENU, ITEMS} from '../../../../utils/apiActions';
 import useNetworkHandling from '../../../../hooks/useNetworkHandling';
 import useNetworkErrorHandling from '../../../../hooks/useNetworkErrorHandling';
 import ProductSkeleton from '../../../../components/skeleton/ProductSkeleton';
 import CustomMenuAccordion from './CustomMenuAccordion';
-import {useAppTheme} from '../../../../utils/theme';
 import ProductSearch from '../../../../components/products/ProductSearch';
 import {getFilterCategory} from '../../../../utils/utils';
+import FBFilter from './FBFilter';
 
 const CancelToken = axios.CancelToken;
 const VegImage = require('../../../../assets/veg.png');
@@ -20,8 +17,7 @@ const NonVegImage = require('../../../../assets/non_veg.png');
 
 const FBProducts = ({provider, domain}: {provider: any; domain: string}) => {
   const {t} = useTranslation();
-  const theme = useAppTheme();
-  const styles = makeStyles(theme.colors);
+  const styles = makeStyles();
   const customMenuSource = useRef<any>(null);
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [menu, setMenu] = useState<any[]>([]);
@@ -104,75 +100,27 @@ const FBProducts = ({provider, domain}: {provider: any; domain: string}) => {
   return (
     <View>
       <View style={styles.filterContainer}>
-        <TouchableOpacity
-          style={[
-            styles.filter,
-            selectedFilter === 'veg' ? styles.selectedFilter : {},
-          ]}
-          onPress={() =>
-            selectedFilter === 'veg'
-              ? setSelectedFilter('')
-              : setSelectedFilter('veg')
-          }>
-          <FastImage source={VegImage} style={styles.filterIcon} />
-          <Text variant={'bodyMedium'} style={styles.filterLabel}>
-            {t('Cart.Veg')}
-          </Text>
-          {selectedFilter === 'veg' && (
-            <Icon
-              name={'clear'}
-              size={20}
-              color={theme.colors.primary}
-              style={styles.icon}
-            />
-          )}
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.filter,
-            selectedFilter === 'nonveg' ? styles.selectedFilter : {},
-          ]}
-          onPress={() =>
-            selectedFilter === 'nonveg'
-              ? setSelectedFilter('')
-              : setSelectedFilter('nonveg')
-          }>
-          <FastImage source={NonVegImage} style={styles.filterIcon} />
-          <Text variant={'bodyMedium'} style={styles.filterLabel}>
-            {t('Cart.Non Veg')}
-          </Text>
-          {selectedFilter === 'nonveg' && (
-            <Icon
-              name={'clear'}
-              size={20}
-              color={theme.colors.primary}
-              style={styles.icon}
-            />
-          )}
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.filter,
-            selectedFilter === 'egg' ? styles.selectedFilter : {},
-          ]}
-          onPress={() =>
-            selectedFilter === 'egg'
-              ? setSelectedFilter('')
-              : setSelectedFilter('egg')
-          }>
-          <FastImage source={NonVegImage} style={styles.filterIcon} />
-          <Text variant={'bodyMedium'} style={styles.filterLabel}>
-            {t('Cart.Egg')}
-          </Text>
-          {selectedFilter === 'egg' && (
-            <Icon
-              name={'clear'}
-              size={20}
-              color={theme.colors.primary}
-              style={styles.icon}
-            />
-          )}
-        </TouchableOpacity>
+        <FBFilter
+          selectedFilter={selectedFilter}
+          label={t('Cart.Veg')}
+          value={'veg'}
+          setSelectedFilter={setSelectedFilter}
+          imageSource={VegImage}
+        />
+        <FBFilter
+          selectedFilter={selectedFilter}
+          label={t('Cart.Non Veg')}
+          value={'nonveg'}
+          setSelectedFilter={setSelectedFilter}
+          imageSource={NonVegImage}
+        />
+        <FBFilter
+          selectedFilter={selectedFilter}
+          label={t('Cart.Egg')}
+          value={'egg'}
+          setSelectedFilter={setSelectedFilter}
+          imageSource={NonVegImage}
+        />
       </View>
       <View style={styles.searchContainer}>
         <ProductSearch
@@ -192,39 +140,13 @@ const FBProducts = ({provider, domain}: {provider: any; domain: string}) => {
   );
 };
 
-const makeStyles = (colors: any) =>
+const makeStyles = () =>
   StyleSheet.create({
     filterContainer: {
       flexDirection: 'row',
       alignItems: 'center',
       marginStart: 16,
       marginTop: 24,
-    },
-    filter: {
-      borderRadius: 60,
-      backgroundColor: colors.neutral100,
-      borderWidth: 1,
-      borderColor: colors.neutral100,
-      paddingVertical: 10,
-      paddingHorizontal: 16,
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginRight: 7,
-    },
-    selectedFilter: {
-      backgroundColor: colors.primary50,
-      borderColor: colors.primary,
-    },
-    filterIcon: {
-      width: 18,
-      height: 18,
-    },
-    filterLabel: {
-      color: colors.neutral400,
-      marginLeft: 4,
-    },
-    icon: {
-      marginLeft: 12,
     },
     searchContainer: {
       marginTop: 24,
