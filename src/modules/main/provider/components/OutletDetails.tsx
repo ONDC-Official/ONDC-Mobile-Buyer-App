5785f8593cdfeaeaeb342ae309dc38b20817ccdf
Punit <psvajpeyi@gmail.com>
2024-08-23 12:25:57 +0530

    chg: navigate to specific map as per user selection

--- src/modules/main/provider/components/OutletDetails.tsx
+++ src/modules/main/provider/components/OutletDetails.tsx
@@ -1,17 +1,13 @@
-import React, {useMemo, useState} from 'react';
-import {
-  Linking,
-  Platform,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import {Text} from 'react-native-paper';
+import React, {useMemo, useRef, useState} from 'react';
+import {Linking, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Divider, Text} from 'react-native-paper';
 import FastImage from 'react-native-fast-image';
 import {useTranslation} from 'react-i18next';
 import {Grayscale} from 'react-native-color-matrix-image-filters';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import {useSelector} from 'react-redux';
 
 import BrandSkeleton from '../../../../components/skeleton/BrandSkeleton';
 import {useAppTheme} from '../../../../utils/theme';
@@ -27,6 +23,21 @@ interface OutletDetails {
 
 const Closed = require('../../../../assets/closed.png');
 const NoImageAvailable = require('../../../../assets/no_store.png');
+const Mappls = require('../../../../assets/maps/mappls.png');
+const Google = require('../../../../assets/maps/google.png');
+
+const getAddressString = (address: any) => {
+  return [
+    address.name,
+    address.street,
+    address.locality,
+    address.city,
+    address.state && `${address.state} - ${address.area_code}`,
+    address.country,
+  ]
+    .filter(value => value) // Filters out null, undefined, or empty string values
+    .join(', ');
+};
 
 const OutletImage = ({source, isOpen}: {source: any; isOpen: boolean}) => {
   const theme = useAppTheme();
@@ -83,6 +94,8 @@ const OutletDetails: React.FC<OutletDetails> = ({
   outlet,
   apiRequested,
 }) => {
+  const {address} = useSelector((state: any) => state.address);
+  const refDirectionSheet = useRef<any>(null);
   const {t} = useTranslation();
   const {convertMinutesToHumanReadable, translateMinutesToHumanReadable} =
     useMinutesToString();
@@ -90,22 +103,7 @@ const OutletDetails: React.FC<OutletDetails> = ({
   const styles = makeStyles(theme.colors);
 
   const getDirection = async () => {
-    const {address} = outlet;
-    const addressString = [
-      address.name,
-      address.street,
-      address.locality,
-      address.city,
-      address.state && `${address.state} - ${address.area_code}`,
-      address.country,
-    ]
-      .filter(value => value) // Filters out null, undefined, or empty string values
-      .join(', ');
-    const url =
-      Platform.OS === 'android'
-        ? `geo:0,0?q=${addressString}`
-        : `maps:0,0?q=${addressString}`;
-    await Linking.openURL(url);
+    refDirectionSheet.current.open();
   };
 
   const {timeToShip, imageSource, outletPhone} = useMemo(() => {
@@ -140,89 +138,140 @@ const OutletDetails: React.FC<OutletDetails> = ({
     return {timeToShip: time, imageSource: source, outletPhone: phone};
   }, [outlet]);
 
+  const navigateToMappls = () => {
+    const destinationAddress = getAddressString(outlet.address);
+    Linking.openURL(
+      `https://www.mappls.com/direction?places=${address.address.lat},${address.address.lng};${outlet?.gps},${destinationAddress}`,
+    );
+  };
+
+  const navigateToMaps = () => {
+    const destinationAddress = getAddressString(outlet.address);
+    Linking.openURL(
+      `https://www.google.com/maps/dir/?api=1&origin=${address.address.lat},${address.address.lng}&destination=${outlet?.gps}&destination_place_id=${destinationAddress}`,
+    );
+  };
+
   if (apiRequested) {
     return <BrandSkeleton />;
   }
 
   return (
-    <View>
-      {!outlet?.isOpen && (
-        <FastImage
-          style={styles.brandImage}
-          source={Closed}
-          resizeMode={FastImage.resizeMode.cover}
-        />
-      )}
-      <View style={styles.brandDetails}>
-        <View style={styles.providerDetails}>
-          <View style={styles.providerLocalityView}>
-            <Text
-              variant={'headlineSmall'}
-              style={styles.title}
-              ellipsizeMode={'tail'}
-              numberOfLines={1}>
-              {provider?.descriptor?.name}
-            </Text>
-            <View style={styles.buttonContainer}>
-              {outletPhone && (
-                <TouchableOpacity
-                  onPress={() => Linking.openURL(`tel: ${outletPhone}`)}
-                  style={styles.actionButton}>
-                  <Icon name={'phone'} color={theme.colors.primary} size={18} />
-                </TouchableOpacity>
-              )}
-              <View style={styles.separator} />
-              <TouchableOpacity
-                style={styles.actionButton}
-                onPress={getDirection}>
-                <Icon
-                  name={'directions'}
-                  color={theme.colors.primary}
-                  size={18}
-                />
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View style={styles.providerLocalityView}>
-            {!!outlet?.address && (
+    <>
+      <View>
+        {!outlet?.isOpen && (
+          <FastImage
+            style={styles.brandImage}
+            source={Closed}
+            resizeMode={FastImage.resizeMode.cover}
+          />
+        )}
+        <View style={styles.brandDetails}>
+          <View style={styles.providerDetails}>
+            <View style={styles.providerLocalityView}>
               <Text
-                variant={'labelLarge'}
-                style={styles.address}
+                variant={'headlineSmall'}
+                style={styles.title}
                 ellipsizeMode={'tail'}
                 numberOfLines={1}>
-                {outlet?.address?.locality
-                  ? `${outlet?.address?.locality},`
-                  : ''}{' '}
-                {outlet?.address?.city}
+                {provider?.descriptor?.name}
               </Text>
-            )}
-          </View>
-          <View style={styles.providerLocalityView}>
-            <MaterialCommunityIcon
-              name={'clock'}
-              size={16}
-              color={theme.colors.neutral200}
-            />
-            <Text variant={'labelLarge'} style={styles.address}>
-              {translateMinutesToHumanReadable(
-                timeToShip.type,
-                timeToShip.time,
-              )}
-            </Text>
-            {!outlet?.isOpen && (
-              <>
-                <View style={styles.dotView} />
-                <Text variant={'labelLarge'} style={styles.address}>
-                  {t('Store.Opens at', {time: outlet?.time_from})}
+              <View style={styles.buttonContainer}>
+                {outletPhone && (
+                  <TouchableOpacity
+                    onPress={() => Linking.openURL(`tel: ${outletPhone}`)}
+                    style={styles.actionButton}>
+                    <Icon
+                      name={'phone'}
+                      color={theme.colors.primary}
+                      size={18}
+                    />
+                  </TouchableOpacity>
+                )}
+                <View style={styles.separator} />
+                <TouchableOpacity
+                  style={styles.actionButton}
+                  onPress={getDirection}>
+                  <Icon
+                    name={'directions'}
+                    color={theme.colors.primary}
+                    size={18}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.providerLocalityView}>
+              {!!outlet?.address && (
+                <Text
+                  variant={'labelLarge'}
+                  style={styles.address}
+                  ellipsizeMode={'tail'}
+                  numberOfLines={1}>
+                  {outlet?.address?.locality
+                    ? `${outlet?.address?.locality},`
+                    : ''}{' '}
+                  {outlet?.address?.city}
                 </Text>
-              </>
-            )}
+              )}
+            </View>
+            <View style={styles.providerLocalityView}>
+              <MaterialCommunityIcon
+                name={'clock'}
+                size={16}
+                color={theme.colors.neutral200}
+              />
+              <Text variant={'labelLarge'} style={styles.address}>
+                {translateMinutesToHumanReadable(
+                  timeToShip.type,
+                  timeToShip.time,
+                )}
+              </Text>
+              {!outlet?.isOpen && (
+                <>
+                  <View style={styles.dotView} />
+                  <Text variant={'labelLarge'} style={styles.address}>
+                    {t('Store.Opens at', {time: outlet?.time_from})}
+                  </Text>
+                </>
+              )}
+            </View>
           </View>
+          <OutletImage source={imageSource} isOpen={outlet?.isOpen} />
         </View>
-        <OutletImage source={imageSource} isOpen={outlet?.isOpen} />
+        <View style={styles.borderBottom} />
       </View>
-      <View style={styles.borderBottom} />
-    </View>
+      <RBSheet
+        ref={refDirectionSheet}
+        height={200}
+        customStyles={{
+          container: styles.rbSheet,
+        }}>
+        <Text variant={'titleMedium'} style={styles.sheetTitle}>
+          {t('Global.Open with')}
+        </Text>
+        <Divider />
+        <TouchableOpacity style={styles.mapRow} onPress={navigateToMappls}>
+          <FastImage
+            style={styles.mapImage}
+            source={Mappls}
+            resizeMode={FastImage.resizeMode.cover}
+          />
+          <Text variant={'bodyMedium'} style={styles.mapName}>
+            Mappls MapmyIndia
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.mapRow} onPress={navigateToMaps}>
+          <FastImage
+            style={styles.mapImage}
+            source={Google}
+            resizeMode={FastImage.resizeMode.cover}
+          />
+          <Text variant={'bodyMedium'} style={styles.mapName}>
+            Maps
+          </Text>
+        </TouchableOpacity>
+      </RBSheet>
+    </>
   );
 };
 
@@ -295,6 +344,29 @@ const makeStyles = (colors: any) =>
     providerDetails: {flex: 1, paddingRight: 24},
     brandImageEmpty: {
       backgroundColor: colors.neutral200,
+    },
+    rbSheet: {
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      paddingHorizontal: 16,
+    },
+    sheetTitle: {
+      color: colors.neutral400,
+      paddingVertical: 16,
+    },
+    mapRow: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: 8,
+    },
+    mapImage: {
+      width: 36,
+      height: 36,
+      marginRight: 12,
+      borderRadius: 8,
+    },
+    mapName: {
+      color: colors.neutral400,
     },
   });
 
